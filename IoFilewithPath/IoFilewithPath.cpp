8b8bb44221853ef5f34d68fd7efd73753e7b0c83
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-21 11:06:07 +0700

    membaca nama file untuk menunjuk ke sebuah file

--- IoFilewithPath/IoFilewithPath.cpp
+++ IoFilewithPath/IoFilewithPath.cpp
@@ -15,4 +15,24 @@ int main() {
 	//menunjuk ke sebuah nama file
 	outfile.open(NamaFile + ".txt", ios::out);
 
-	
+	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimeted loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendaoatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup  file nya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//memnunjuk ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+
