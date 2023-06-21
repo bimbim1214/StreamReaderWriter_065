5f0f075c359a27a35f8bec0c6653bd2c10cf90a6
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-21 11:34:43 +0700

    membuat perintah awal program

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -2,3 +2,8 @@
 #include <exception>
 //untuk objek exception yang akan digunakan
 #include <array>
+//untuk objek array yang akan kita gunakan
+using namespace std;
+int main() {
+	cout << "awal program" << endl; //penanda 1:...
+}
