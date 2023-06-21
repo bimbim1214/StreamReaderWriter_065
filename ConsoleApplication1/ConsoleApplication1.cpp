737115fd633570ea79150036fdf11377a46e1253
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-21 10:44:24 +0700

    perulangan akan berenti juka anda memasukan karakter q

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -18,3 +18,5 @@ int main() {
 		cout << "- ";
 		//mendaoatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
