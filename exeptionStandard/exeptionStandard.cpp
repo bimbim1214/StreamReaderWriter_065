8db061535764af04b97a8efb3c2c6cadd2d1b020
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-21 11:42:26 +0700

    menambhkan try dan catch

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -6,4 +6,18 @@
 using namespace std;
 int main() {
 	cout << "awal program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+	cout << "baris program yang terakhir" << endl;
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
 }
